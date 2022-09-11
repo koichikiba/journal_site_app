bdca8b2f983eac35a34859de772070bed45fec0f
koichikiba <backy.0710@gmail.com>
2022-09-11 12:19:43 +0900

    論文一覧画面のview作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         // ArticleSeeder（シーダークラス）の呼び出し
-+       $this->call(ArticleSeeder::class);
+        $this->call(ArticleSeeder::class);
     }
 }
